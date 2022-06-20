c62b0b9176111c4c444b04983089a0147b2a5450
Dylan McKendry <dylan.mckendry@outlook.com>
2022-06-21 01:41:14 +1000

    Update polynomial.py

--- polynomial.py
+++ polynomial.py
@@ -17,19 +17,22 @@ def _coeff_mat(x, deg):
         for n in range(2, deg + 1):
             mat_[:, n] = x**n
     return mat_
-    
+
+
 @nb.jit
 def _fit_x(a, b):
     # linalg solves ax = b
     det_ = np.linalg.lstsq(a, b)[0]
     return det_
  
- @nb.jit
+    
+@nb.jit
 def fit_poly(x, y, deg):
     a = _coeff_mat(x, deg)
     p = _fit_x(a, y)
     # Reverse order so p[0] is coefficient of highest order
     return p[::-1]
+
 
 @nb.jit
 def eval_polynomial(P, x):
