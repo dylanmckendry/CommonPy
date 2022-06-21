9f97ab929df3cb15f481018a83997d7e60153767
Dylan McKendry <dylan.mckendry@outlook.com>
2022-06-21 22:28:34 +1000

    Update polynomial.py

--- polynomial.py
+++ polynomial.py
@@ -1,47 +1,45 @@
-# Load relevant libraries
 import numpy as np
 import numba as nb
-import matplotlib.pyplot as plt
-
-# Goal is to implement a numba compatible polyfit (note does not include error handling)
 
 # Define Functions Using Numba
 # Idea here is to solve ax = b, using least squares, where a represents our coefficients e.g. x**2, x, constants
 @nb.njit
 def _coeff_mat(x, deg):
-    mat_ = np.zeros(shape=(x.shape[0],deg + 1))
-    const = np.ones_like(x)
-    mat_[:,0] = const
+
+    mat_ = np.ones(shape=(x.shape[0],deg + 1))
     mat_[:, 1] = x
+
     if deg > 1:
         for n in range(2, deg + 1):
-            mat_[:, n] = x**n
-    return mat_
+            # here, the pow()-function was turned into multiplication, which gave some speedup for me (up to factor 2 for small degrees, ...
+            # ... which is the normal application case)
+            mat_[:, n] = mat_[:, n-1] * x
 
+    # evaluation of the norm of each column by means of a loop
+    scale_vect = np.empty((deg + 1, ))
+    for n in range(0, deg + 1):
+        
+        # evaluation of the column's norm (stored for later processing)
+        col_norm = np.linalg.norm(mat_[:, n])
+        scale_vect[n] = col_norm
+        
+        # scaling the column to unit-length
+        mat_[:, n] /= col_norm
 
+    return mat_, scale_vect
+    
 @nb.jit
-def _fit_x(a, b):
+def _fit_x(a, b, scales):
     # linalg solves ax = b
     det_ = np.linalg.lstsq(a, b)[0]
+    # due to the stabilization, the coefficients have the wrong scale, which is corrected now
+    det_ /= scales
+
     return det_
- 
-    
+
 @nb.jit
 def fit_poly(x, y, deg):
-    a = _coeff_mat(x, deg)
-    p = _fit_x(a, y)
+    a, scales_ = _coeff_mat(x, deg)
+    p = _fit_x(a, y, scales_)
     # Reverse order so p[0] is coefficient of highest order
     return p[::-1]
-
-
-@nb.jit
-def eval_polynomial(P, x):
-    '''
-    Compute polynomial P(x) where P is a vector of coefficients, highest
-    order coefficient at P[0].  Uses Horner's Method.
-    '''
-    result = 0
-    for coeff in P:
-        result = x * result + coeff
-    return result
-    
